9b794203f1b5015241a8462e64163321a8726b22
Colt Thatcher <colt.thatcher303@gmail.com>
2021-10-21 17:09:03 -0700

    Add README
    Co-authored by: Anna Pittman <annapittman@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const person1Input = $("input#person1").val();
+  
+  $(".person1").text(person1Input);
+  
+  $("#letter").show();
+  
+  });
+});
